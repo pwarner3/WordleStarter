e5907fd61ac674573877b2f4338f47a0a7ca55a7
Parker Warner <plouwarner@gmail.com>
2023-09-14 15:25:44 -0600

    Improve Unit tests

--- test_wordle.py
+++ test_wordle.py
@@ -1,13 +1,21 @@
 import unittest
+import random
 from WordleDictionary import FIVE_LETTER_WORDS
 
-class TestRandomWordSelector(unittest.TestCase):
-    def test_dictionary(self):
-        word = "aahed"
-        test = FIVE_LETTER_WORDS[0]
-        self.assertEqual(word,test,"Should be equal")
-        self.assertIn("abort",FIVE_LETTER_WORDS)
 
 class TestWords(unittest.TestCase):
-    def test_checkRealWord(self):
-        self.assertEqual(1,1,"Test")
+    def test_dictionaryHasWord(self):
+        word = "aahed"
+        test = FIVE_LETTER_WORDS[0]
+        self.assertEqual(word, test, "Should be equal")
+        self.assertIn(word, FIVE_LETTER_WORDS)
+
+    def test_dictionaryWithoutWord(self):
+        nonWord = "aaaaa"
+        self.assertNotIn(nonWord, FIVE_LETTER_WORDS)
+
+
+class TestRandomWordSelector(unittest.TestCase):
+    def test_checkRandomWord(self):
+        randomWord = random.choice(FIVE_LETTER_WORDS)
+        self.assertIn(randomWord, FIVE_LETTER_WORDS, "Random word in list")
