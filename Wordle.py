6643e40464e15b6feba76e16cec5d11e10f5599d
Parker Warner <plouwarner@gmail.com>
2023-09-22 14:00:32 -0600

    Refactor incrementing, Add hard mode fix, Fix end

--- Wordle.py
+++ Wordle.py
@@ -19,11 +19,13 @@ def wordle():
     #Selecting the random word
     randomWord = random.choice(FIVE_LETTER_WORDS).upper()
     hardWord = []
-    for x in randomWord:
+    for x in randomWord: # Initialize hard mode word
         hardWord.append("")
 
     def enter_action(s):
+
         if gw.hard_mode_status() == False: # Regular Mode
+            rowDecrement = 0
             word = ""
             for x in range(N_COLS): 
                 word += gw.get_square_letter(gw.get_current_row(), x)
@@ -40,35 +42,49 @@ def wordle():
                         gw.set_square_color(gw.get_current_row(), x, PRESENT_COLOR)
                     else: 
                         gw.set_square_color(gw.get_current_row(), x, MISSING_COLOR)
-                temp = gw.get_current_row() + 1
-                gw.set_current_row(temp)
 
             else:
                 gw.show_message("Not a word, try again")
+                rowDecrement = 1
 
-            if word == randomWord:
-                #gw.show_message("Congrats!!! You Win!!")
+            if word == randomWord: # Win Condition
                 
-                numTries = gw.get_current_row()
+                numTries = gw.get_current_row() + 1
                 global pw
                 pw = GameEndWindow(numTries) 
 
                 pw.show_message("Congrats! It took you " + str(numTries) + " attempt(s)!\n \nScreenshot to share with friends")
                 
                 for x in range(0, numTries):
-                    for y in range(0, 5):
+                    for y in range(N_COLS):
                         color = gw.get_square_color(x, y)
                         pw.set_square_color(x, y, color)
+                return
+            
+            temp = gw.get_current_row() + 1 - rowDecrement
+            if temp == N_ROWS: # Lose Condition
+                numTries = gw.get_current_row() + 1
+                global lw
+                lw = GameEndWindow(numTries) 
 
+                lw.show_message("Sorry, You Lost. It took you " + str(numTries) + " attempt(s)!\n \nShare your results with friends!!!")
+                
+                for x in range(0, numTries):
+                    for y in range(N_COLS):
+                        color = gw.get_square_color(x, y)
+                        lw.set_square_color(x, y, color)
+                return
+            gw.set_current_row(temp)
             
         else: # Hard Mode
+            rowDecrement = 0
             word = ""
             for x in range(N_COLS):
                 word += gw.get_square_letter(gw.get_current_row(), x)
                 if hardWord[x] != "":
-                    if word[x] != hardWord[x]: # Check that letter submitted matches letters found (issue)
+                   if word[x] != hardWord[x]: # Check that letter submitted matches letters found
                         gw.show_message("Must use found correct letters","Red")
-                        return
+                        rowDecrement = 1
 
             #Validating if it is a word
             if word.lower() in FIVE_LETTER_WORDS:
@@ -82,39 +98,51 @@ def wordle():
                     elif currentLetter in randomWord:
                         gw.set_square_color(gw.get_current_row(), x, PRESENT_COLOR)
                     else: 
-                        gw.set_square_color(gw.get_current_row(), x, MISSING_COLOR)
-                temp = gw.get_current_row() + 1
-                gw.set_current_row(temp)
-
-                for x in range(N_COLS):
-                    if "".join(hardWord) == word:
-                        continue
-                    if hardWord[x] != "":
-                        gw.set_square_letter(gw.get_current_row(), x,hardWord[x])
-                        gw.set_square_color(gw.get_current_row(), x,CORRECT_COLOR)
+                        gw.set_square_color(gw.get_current_row(), x, MISSING_COLOR)               
 
             else:
                 gw.show_message("Not a word, try again")
+                rowDecrement = 1
 
-            if word == randomWord:
-                gw.show_message("Congrats!!! You Win!!")
-                numTries = gw.get_current_row()
+            if word == randomWord: # Win Condition
+
+                numTries = gw.get_current_row() + 1
                 global mw
                 mw = GameEndWindow(numTries) 
 
                 mw.show_message("Congrats! It took you " + str(numTries) + " attempt(s)!\n \nShare your results with friends!!!")
                 
                 for x in range(0, numTries):
-                    for y in range(0, 5):
+                    for y in range(N_COLS):
                         color = gw.get_square_color(x, y)
                         mw.set_square_color(x, y, color)
 
+            temp = gw.get_current_row() + 1 - rowDecrement # Incrememnt row to be new line (or stay on same one)
+            if temp == N_ROWS: # Lose Condition
+                numTries = gw.get_current_row() + 1
+                global lhw
+                lhw = GameEndWindow(numTries) 
+
+                lhw.show_message("Sorry, You Lost. It took you " + str(numTries) + " attempt(s)!\n \nShare your results with friends!!!")
+                
+                for x in range(0, numTries):
+                    for y in range(N_COLS):
+                        color = gw.get_square_color(x, y)
+                        lhw.set_square_color(x, y, color)
+                return
+            gw.set_current_row(temp)
+
+            for x in range(N_COLS):
+                
+                if "".join(hardWord) == word:
+                    continue
+                if hardWord[x] != "":
+                    gw.set_square_letter(gw.get_current_row(), x,hardWord[x])
+                    gw.set_square_color(gw.get_current_row(), x,CORRECT_COLOR)
+                    
+                    
+
     gw = WordleGWindow()
-    
-    # y = 0
-    # for x in randomWord:
-    #     gw.set_square_letter(N_ROWS-6, y ,randomWord[y])
-    #     y += 1
 
     gw.show_message(f"Word is: {randomWord}")
 
